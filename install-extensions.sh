7b2e2784c451f3e624840854c9424e5f3d08829a
Odusanya <odusanya18@gmail.com>
2017-11-15 12:00:51 +0100

    gmp shell script

---
+++ install-extensions.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+gmpLoad=php -r "print_r(extension_loaded('gmp'));" 2>/dev/null
+
+if [ gmpLoad = 1 ]; then
+
+	if [ sudo add-apt-repository 2>/dev/null ]; then
+
+    	sudo apt-get install -y python-software-properties
+    else
+    	continue
+	fi
+
+	sudo add-apt-repository -y ppa:ondrej/php
+	sudo apt update -y
+	sudo apt install php-gmp -y
+
+else
+	continue
+fi
