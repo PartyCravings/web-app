824df9cf07ef9b24809102fc041045d1a21f00b8
Odusanya18 <odusanya18@gmail.com>
2017-12-10 22:57:11 +0100

    gae mods

--- install-extensions.sh
+++ install-extensions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-(git clone https://github.com/PartyCravings/php-pm && cd php-pm/ && composer require php-pm/httpkernel-adapter:dev-master)
+(git clone https://github.com/PartyCravings/php-pm && cd php-pm/ && composer require partycravings/httpkernel-adapter:dev-master)
