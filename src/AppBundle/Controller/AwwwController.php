86fffb2ea5932d5b8620d48274e44528c0939229
Odusanya18 <odusanya18@gmail.com>
2017-12-15 02:31:48 +0100

    gae mods

--- src/AppBundle/Controller/AwwwController.php
+++ src/AppBundle/Controller/AwwwController.php
@@ -13,24 +13,6 @@ use Doctrine\ORM\EntityManagerInterface;
 class AwwwController extends AbstractController //Purposely named to match first
 {
     /**
-     * @Route("/{req}", name="redirect", host="www.%hostname%", requirements={"req": ".+"})
-     * @Template(":default:countries.html.twig")
-     */
-    public function redirectAction(Request $request, EntityManagerInterface $em) :void
-    {
-        $path = $request->getBasePath();
-        $countryName = $request->headers->get("HTTP_CF_IPCOUNTRY");
-        if ('XX' == $countryName || empty($countryName)) {
-            $countryName = geoip_country_code_by_name($request->getClientIp());
-        }
-        $country = $em->getRepository(Country::class)->findByName($country);
-        if ($country) {
-            $uri = $request->getScheme().'://'.$country->hostname ?: $country->subdomain.'/'.$path;
-            $this->redirect($uri);
-        }
-    }
-
-    /**
      * @Route("/sitemap.xml", name="sitemap", host="www.%hostname%", defaults={"page": "1", "_format"="xml"})
      * @ParamConverter("countries", class="AppBundle:Country", options={"id"="_country", "repository_method"= "findAll"})
      * @Template(":default:sitemap.xml.twig", vars={"countries"})
@@ -38,6 +20,24 @@ class AwwwController extends AbstractController //Purposely named to match first
      */
     public function sitemapAction() :void
     {
+    }
+
+    /**
+     * @Route("/{req}", name="redirect", requirements={"req": ".+"})
+     * @Template(":default:countries.html.twig")
+     */
+    public function redirectAction(Request $request, EntityManagerInterface $em) :void
+    {
+        $path = $request->getBasePath();
+        $countryName = $request->headers->get('HTTP_CF_IPCOUNTRY');
+        /*if ('XX' == $countryName || empty($countryName)) {
+            $countryName = geoip_country_code_by_name($request->getClientIp());
+        }*/
+        $country = $em->getRepository(Country::class)->findByName($country);
+        if ($country) {
+            $uri = $request->getScheme().'://'.$country->hostname ?: $country->subdomain.'/'.$path;
+            $this->redirect($uri);
+        }
     }
 
     /**
